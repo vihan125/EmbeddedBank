73631acfbc1682e47a4e87c649c3b09efdc59cd5
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2019-12-01 16:40:10 +0530

    removed auth in Main

--- src/main/java/EmbeddedBank.java
+++ src/main/java/EmbeddedBank.java
@@ -4,14 +4,14 @@ import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.ResourceConfig;
 
 import resource.MyResourceConfig;
-import util.authService;
+//import util.authService;
 
 
 public class EmbeddedBank {
 
 	public static void main(String[] args) {
         ResourceConfig resourceConfig = new MyResourceConfig();
-        authService n = new authService();
+        //authService n = new authService();
         try {
         	
             GrizzlyServerFactory.createHttpServer(URI.create("http://localhost:6973"), resourceConfig);   
